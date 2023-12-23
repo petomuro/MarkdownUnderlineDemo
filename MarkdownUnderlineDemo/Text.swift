f06f1131d3c6c0fc1e70783d554b65cb6af68d8f
Peter Murín <peter.murin@codium.sk>
2023-12-23 18:36:15 +0100

    Update

--- MarkdownUnderlineDemo/Text.swift
+++ MarkdownUnderlineDemo/Text.swift
@@ -5,4 +5,29 @@
 //  Created by Peter Murín on 23/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+public extension Text {
+    init(underlineMarkdown: String) {
+        self = Text.parseMarkdown(underlineMarkdown)
+    }
+
+    private static func parseMarkdown(_ underlineMarkdown: String) -> Text {
+        var components: [Text] = []
+        let lines = underlineMarkdown.components(separatedBy: "\n")
+
+        for line in lines {
+            let componentsInLine = line.components(separatedBy: "__")
+
+            for (index, component) in componentsInLine.enumerated() {
+                if index % 2 == 0 {
+                    components.append(Text(.init(component)))
+                } else {
+                    components.append(Text(.init(component)).underline())
+                }
+            }
+        }
+
+        return components.reduce(Text("")) { $0 + $1 + Text("") }
+    }
+}
