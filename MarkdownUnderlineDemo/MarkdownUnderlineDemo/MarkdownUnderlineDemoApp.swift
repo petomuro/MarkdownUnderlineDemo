e18e4eecd8b73a3f9ad72b550a0786b79f8ef915
Peter Murín <peter.murin@codium.sk>
2023-12-23 18:34:35 +0100

    Update

---
+++ MarkdownUnderlineDemo/MarkdownUnderlineDemo/MarkdownUnderlineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownUnderlineDemoApp.swift
+//  MarkdownUnderlineDemo
+//
+//  Created by Peter Murín on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownUnderlineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownUnderlineDemo/Text.swift
@@ -0,0 +1,8 @@
+//
+//  Text.swift
+//  MarkdownUnderlineDemo
+//
+//  Created by Peter Murín on 23/12/2023.
+//
+
+import Foundation
